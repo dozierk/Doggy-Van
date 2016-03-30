a07203cd788367535ee68a92b594735884de50b6
kayla dozier <kayla.dozier82@gmail.com>
2016-03-29 19:35:44 -0700

    Initial commits

---
+++ Doggy Van/Animation.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Doggy Van
+//
+//  Created by kayla dozier on 3/7/16.
+//  Copyright © 2016 kayla dozier. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+
+class Animation {
+    
+    var audioPlayer = AVAudioPlayer()
+    
+}
+
---
+++ Doggy Van/GameController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  Doggy Van
+//
+//  Created by kayla dozier on 2/12/16.
+//  Copyright © 2016 kayla dozier. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+class GameController: UIViewController {
+
+    
+    @IBOutlet var Doggy: UIImageView!
+    
+    @IBOutlet weak var changeLabel: UILabel!
+    
+    
+    var gameString = ["dance", "eat", "drive", "run", "play", "sing", "sleep", "imagine", "beautiful", "laughing", "smiling", "crying", "jumping", "dolphin", "cat", "apple", "tree", "bird"]
+   
+    label.text = gameString[0]
+    
+    let gameString
+    
+    myStrings.first
+
+    }
+
+
+    @IBAction func Verb(sender: UIButton) {
+        
+    }
+    
+    @IBAction func Adjective(sender: UIButton) {
+        
+    }
+
+    @IBAction func Noun(sender: UIButton) {
+        
+        AVAudioPlayer.valueForKey("let dog bark when answer is incorrect")
+
+}
