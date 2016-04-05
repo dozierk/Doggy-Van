8a4069d11fe13e04359259506c9038d61e0f3c9f
kayla dozier <kayla.dozier82@gmail.com>
2016-04-04 19:15:41 -0700

    Added a UIButton and comments. Once the new button is pressed, the label will change words.

--- Doggy Van/GameController.swift
+++ Doggy Van/GameController.swift
@@ -17,18 +17,18 @@ class GameController: UIViewController {
     
     @IBOutlet weak var changeLabel: UILabel!
     
-    // These are the word options that appear in the UILabel
     var gameString = ["dance", "eat", "drive", "run", "play", "sing", "sleep", "imagine", "beautiful", "laughing", "smiling", "crying", "jumping", "dolphin", "cat", "apple", "tree", "bird"]
-   
- //   label.text = gameString[0]
+    
+  //  label.text = gameString[0]
     
   //  let gameString
     
+  //  myStrings.first
     
     
 
 
-    // The verb, adjective, and noun are the only options that could pertain to the label
+    // The verb, adjective, and noun are the only options that could pertain to the label.
     
     @IBAction func Verb(sender: UIButton) {
         
@@ -44,10 +44,17 @@ class GameController: UIViewController {
 
     }
  
-    // When the next button is pressed, the text in the label will change
-    
+    // When the next button is pressed, the text in the label will change.
     @IBAction func buttonPressed(sender: UIButton) {
         changeLabel.text = "dance", "eat", "drive", "run", "play", "sing", "sleep", "imagine", "beautiful", "laughing", "smiling", "crying", "jumping", "dolphin", "cat", "apple", "tree", "bird"
     }
+
+    
+    // Override to allow for dragging the button.
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        super.touchesMoved(touches, withEvent: event)
+        
+        
+}
 
 }
