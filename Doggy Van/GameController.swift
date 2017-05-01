eb1f54a5369f37c2435b9fa88b4b77d4c427a0c3
kayla dozier <kayla.dozier82@gmail.com>
2017-05-01 16:50:27 -0700

    Project location updates. Adding dictionary branch to push.

--- Doggy Van/GameController.swift
+++ Doggy Van/GameController.swift
@@ -9,28 +9,107 @@
 import UIKit
 import AVFoundation
 import CoreGraphics
+import GameKit
 
 
 /** The primary ViewController for the Game. */
 class GameController: UIViewController {
     
+    //Universal conditional statement created for Verb, Adjective, and Noun actions. The logic used checks if the answer is correct or incorrect and ends the game once all 30 words have been answered.
+    func answerFeedback(isCorrect: Bool) {
+        
+        VerbButton.isEnabled = false
+        AdjectiveButton.isEnabled = false
+        NounButton.isEnabled = false
+        NextButton.isHidden = false
+        
+        let currentWord = words[currentWordIndex]
+        
+        if currentWord.string == words.last?.string {
+            
+            gameOver()
+            
+        }
+        
+        
+        if isCorrect {
+            
+            answerCorrect.text = "CORRECT"
+            answerCorrect.isHidden = false
+            answerIncorrect.isHidden = true
+            
+        } else {
+            
+            answerIncorrect.text = "INCORRECT"
+            answerIncorrect.isHidden = false
+            answerCorrect.isHidden = true
+        }
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        currentWordIndex = 0
+         words = Source.getWords(amount: 30)
+    }
+    
+    
+    //Func created to end game.
+    func gameOver() {
+        
+        performSegue(withIdentifier: "Done", sender: nil)
+        
+    }
+    
+    @IBOutlet weak var VerbButton: UIButton!
+    
+    @IBOutlet weak var AdjectiveButton: UIButton!
+    
+    @IBOutlet weak var NounButton: UIButton!
+    
+    @IBOutlet weak var NextButton: UIButton!
+    
+    
     /** Local array of words (temporarily) being used as a source for the Test. */
-    let words = Source.getWords(amount: 30)
+    var words = Source.getWords(amount: 30)
     
     /** Tracks the index of the global Array used as a Model Object.
      *
      * Functional: Whenever the index is changed, the label text is updated to reflect it.
      */
     var currentWordIndex = 0 {
+        
         willSet {
+            
             changeLabel.text = words[newValue].string
+            
         }
     }
+    
     
     @IBOutlet weak var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //    authenticateLocalPlayer ()
+        //
+        //    }
+        //    //Initiate Gamecenter
+        //    func authenticateLocalPlayer () {
+        //
+        //        var localPlayer = GKLocalPlayer.localPlayer()
+        //
+        //        localPlayer.authenticateHandler = {(GameController, error) -> Void in
+        //
+        //        if (GameController != nil) {
+        //            self.presentViewController(GameController, animated) true, completion: nil
+        //        } else {
+        //            print(GKLocalPlayer.localPlayer().authenticated()
+        //            }
+        //        }
+        //    }
         
         // Set the initial value of the label text.
         changeLabel.text = words[currentWordIndex].string
@@ -54,76 +133,37 @@ class GameController: UIViewController {
     @IBOutlet weak var changeLabel: UILabel!
     
     
-    
     //    All @IBActions contain similar conditional statements that recognize whether the word is a verb, adjective, or noun.
     @IBAction func Verb(_ sender: UIButton) {
         
         let currentWord = words[currentWordIndex]
         
-        print (currentWord.type)
+        print (currentWord.type as Any)
         
-        if currentWord.isVerb {
-            
-            //            print("correct")
-            answerCorrect.text = "CORRECT"
-            answerCorrect.isHidden = false
-            
-        } else {
-            
-            //            print("incorect")
-            answerIncorrect.text = "INCORRECT"
-            answerIncorrect.isHidden = false
-        }
-        
+        answerFeedback(isCorrect: currentWord.isVerb)
     }
     
-    //        func puzzleEnded(solved: Bool) {
-    //            timer.invalidate()
-    //            gameSpace.userInteractionEnabled = false
-    //            UIView.animateWithDuration(0.4) { () -> Void in
-    //                self.view.backgroundColor = solved ? UIColor.greenColor() : UIColor.redColor()
     
     @IBAction func Adjective(_ sender: UIButton) {
         
-        
         let currentWord = words[currentWordIndex]
         
-        print (currentWord.type)
+        print (currentWord.type as Any)
         
-        if currentWord.isAdjective {
-            
-            //            print("correct")
-            answerCorrect.text = "CORRECT"
-            answerCorrect.isHidden = false
-            
-        } else {
-            
-            //            print("incorect")
-            answerIncorrect.text = "INCORRECT"
-            answerIncorrect.isHidden = false
-        }
+        answerFeedback(isCorrect: currentWord.isAdjective)
         
     }
     
     @IBAction func Noun(_ sender: UIButton) {
+        
         let currentWord = words[currentWordIndex]
         
-        print (currentWord.type)
+        print (currentWord.type as Any)
         
-        if currentWord.isNoun {
-            
-            //            print("correct")
-            answerCorrect.text = "CORRECT"
-            answerCorrect.isHidden = false
-            
-        } else {
-            
-            //            print("incorect")
-            answerIncorrect.text = "INCORRECT"
-            answerIncorrect.isHidden = false
-        }
-        
+        answerFeedback(isCorrect: currentWord.isNoun)
     }
+    
+    
     
     @IBOutlet weak var answerCorrect: UILabel!
     
@@ -134,18 +174,23 @@ class GameController: UIViewController {
     @IBAction func NextWord(_ sender: UIButton) {
         
         // Increments the index that we're referencing in the global Array constant.  Modulo/Remainder is set for the total amount in the Array so the index will never go out of range, it will just go back to 0 and continue incrementing.
-        currentWordIndex = (currentWordIndex + 1) % words.count
+        currentWordIndex = (currentWordIndex + 1)
         
         print("The list contains \(words.count) 30 words.")
         
         answerCorrect.isHidden = true
         answerIncorrect.isHidden = true
+        VerbButton.isEnabled = true
+        AdjectiveButton.isEnabled = true
+        NounButton.isEnabled = true
+        sender.isHidden = true
         
-        //       changeLabel.layer.cornerRadius = 2
-        //       changeLabel.layer.borderColor = UIColor.grayColor().CGColor
-        //       changeLabel.layer.borderWidth = 2.0
+        
+        
+        
     }
     
-    
 }
+
+
 
--- Doggy Van/WordSource.swift
+++ Doggy Van/WordSource.swift
@@ -7,18 +7,71 @@
 //
 
 import Foundation
+//import Alamofire
+
 
 /** A utility class made to provide the words for the game.  This class will likely be where Dictionary APIs get implemented to replace this temporary manual encapsulation of words.*/
+
 class Source {
     class func getWords(amount: Int) -> [Word] {
         
-        let verbs : [Word] = [Verb(string: "Dance"), Verb(string: "Slide"), Verb(string: "Bounce"), Verb(string: "Scream"), Verb(string: "Eat"), Verb(string: "Jump"), Verb(string: "Sleep"), Verb(string: "Play"), Verb(string: "Carry"), Verb(string: "Sing")]
-        let adjs : [Word] = [Adjective(string: "Small"), Adjective(string: "Big"), Adjective(string: "Loud"), Adjective(string: "Quiet"), Adjective(string: "Empty"), Adjective(string: "Angry"), Adjective(string: "Last"), Adjective(string: "Fast"), Adjective(string: "Long"), Adjective(string: "Wet")]
-        let nouns : [Word] = [Noun(string: "Balloon"), Noun(string: "Dress"), Noun(string: "Book"), Noun(string: "Airplane"), Noun(string: "Ball"), Noun(string: "Cat"), Noun(string: "Dog"), Noun(string: "Apple"), Noun(string: "Girl"), Noun(string: "Boy")]
+        let verbs : [Word] = [Verb(string: "DANCE"), Verb(string: "SLIDE"), Verb(string: "BOUNCE"), Verb(string: "SCREAM"), Verb(string: "EAT"), Verb(string: "JUMP"), Verb(string: "SLEEP"), Verb(string: "PLAY"), Verb(string: "CARRY"), Verb(string: "SING")]
+        let adjs : [Word] = [Adjective(string: "SMALL"), Adjective(string: "BIG"), Adjective(string: "LOUD"), Adjective(string: "QUIET"), Adjective(string: "EMPTY"), Adjective(string: "ANGRY"), Adjective(string: "LAST"), Adjective(string: "FAST"), Adjective(string: "LONG"), Adjective(string: "WET")]
+        let nouns : [Word] = [Noun(string: "BALLOON"), Noun(string: "DRESS"), Noun(string: "BOOK"), Noun(string: "AIRPLANE"), Noun(string: "BALL"), Noun(string: "CAT"), Noun(string: "DOG"), Noun(string: "APPLE"), Noun(string: "GIRL"), Noun(string: "BOY")]
         
         return (verbs + adjs + nouns).shuffle()
+        
+//        Alamofire.request("https:od-api.oxforddictionaries.com/api/v1")
     }
 }
+// Dictionary API used for extending word source and replace the initial encapsulation of words. 
+
+func wordOxford() {
+    // TODO: replace with your own app id and app key
+//    let appId = "69682fbe"
+//    let appKey = "7f8c6d93fef65509c06235eba8997239"
+//    let language = "en"
+//    let filters = "registers=Rare;domains=Art"
+    let appId = "Doggy Van"
+    let appKey = "7f8c6d93fef65509c06235eba8997239"
+    let language = "en"
+    let filters = "registers=Rare;domains=Art"
+    let url = URL(string: "https://od-api.oxforddictionaries.com:443/api/v1/wordlist/\(language)/\(filters)")!
+    var request = URLRequest(url: url)
+    request.addValue("application/json", forHTTPHeaderField: "Accept")
+    request.addValue(appId, forHTTPHeaderField: "app_id")
+    request.addValue(appKey, forHTTPHeaderField: "app_key")
+    
+    let session = URLSession.shared
+    _ = session.dataTask(with: request, completionHandler: { data, response, error in
+        if let response = response,
+            let data = data,
+            let jsonData = try? JSONSerialization.jsonObject(with: data, options: .mutableContainers) {
+            print(response)
+            print(jsonData)
+        } else {
+            print(error)
+            print(String.init(data: data!, encoding: String.Encoding(rawValue: String.Encoding.utf8.rawValue)))
+        }
+        
+    }).resume()
+}
+
+
+//Alamofire.request("URL GOES HERE \(language)/\(filters)
+//    ").responseJSON { response in print(response.request) // original URL request print(response.response) // HTTP URL response print(response.data) // server data print(response.result) // result of response serialization if let JSON = response.result.value { print("//JSON: \(JSON)") } }
+
+
+//    Alamofire.request("https://httpbin.org/get").responseJSON { response in
+//    print(response.request)  // original URL request
+//    print(response.response) // HTTP URL response
+//    print(response.data)     // server data
+//    print(response.result)   // result of response serialization
+    
+//    if let JSON = response.result.value {
+//    print("JSON: \(JSON)")
+//    }
+//}
 
 extension Collection {
     
@@ -30,6 +83,8 @@ extension Collection {
     }
 }
 
+
+//
 extension MutableCollection where Index == Int {
     /// Shuffle the elements of `self` in-place.
     mutating func shuffleInPlace() {
